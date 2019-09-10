77f74f7bee631625ce167a212e4ebe8356b87704
Leonardo Situmorang <leonardo.situmorang@gmail.com>
2019-09-10 14:54:23 +0700

    Add tmux configuration

--- 00-zsh-vim-vundle-git-tmux.sh
+++ 00-zsh-vim-vundle-git-tmux.sh
@@ -10,7 +10,10 @@ echo "Setting up ~/.ssh and ~/.ssh/authorized_keys"
 cd ~
 mkdir .ssh
 chmod 700 .ssh
-curl -s https://gist.githubusercontent.com/killedbymemory/26243a397d9b2366540502e967a9622c/raw/60ee88b559b7a1a8b078a7acbe0740543cd4202c/id_rsa.pub >> ~/.ssh/authorized_keys
+## -s for silent
+## -L for following redirect
+## -o for output file
+curl -sLo ~/.ssh/authorized_keys https://gist.githubusercontent.com/killedbymemory/26243a397d9b2366540502e967a9622c/raw/60ee88b559b7a1a8b078a7acbe0740543cd4202c/id_rsa.pub
 chmod 600 ~/.ssh/authorized_keys 
 
 
@@ -26,7 +29,10 @@ echo "Installing Git"
 sudo apt -y install git
 
 echo "Setting up Git configuration"
-curl -s https://gist.githubusercontent.com/killedbymemory/753b50bd053808936fa18516e0b6f44f/raw/58887028007dd4f539abef8221dbdbf334332679/.gitconfig -o ~/.gitconfig
+## -s for silent
+## -L for following redirect
+## -o for output file
+curl -sLo ~/.gitconfig https://gist.githubusercontent.com/killedbymemory/753b50bd053808936fa18516e0b6f44f/raw/58887028007dd4f539abef8221dbdbf334332679/.gitconfig
 
 
 ## Vundle && Vim && Install plugins from CLI
@@ -36,9 +42,14 @@ mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "Setting up Vim configuration ~/.vimrc"
-curl -s https://gist.githubusercontent.com/killedbymemory/c1b8825c55c0551ed0f273400318a1ca/raw/2506a01be24cb5f0fbca5de62a180477609fbeba/.vimrc -o ~/.vimrc
-# Install plugins from CLI, unfortunately unavailable colorscheme prompt user response ;(
-# vim +PluginInstall +qall
+## -s for silent
+## -L for following redirect
+## -o for output file
+curl -sLo ~/.vimrc https://gist.githubusercontent.com/killedbymemory/c1b8825c55c0551ed0f273400318a1ca/raw/2506a01be24cb5f0fbca5de62a180477609fbeba/.vimrc
+
+# Install plugins from CLI.
+# Unfortunately unavailable colorscheme prompt user response ;(
+vim +PluginInstall +qall
 
 ## Docker
 ## See: https://docs.docker.com/install/linux/docker-ce/ubuntu/
@@ -91,3 +102,11 @@ sudo apt -y install zsh
 echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo "Remember to enable oh-my-zsh plugins in ~/.zshrc!"
+
+
+## tmux
+echo "Setting up tmux configuration ~/.tmux.conf"
+## -s for silent
+## -L for following redirect
+## -o for output file
+curl -sLo ~/.tmux.conf https://gist.github.com/killedbymemory/6b474c113ab420985191222ce51f0265/raw/017423c0714b4886c6566face4cbb095e7b398b5/.tmux.conf
